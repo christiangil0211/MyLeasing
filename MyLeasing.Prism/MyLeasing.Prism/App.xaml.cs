509a067cb4dee9def166f0b08140dbfe16a0e820
Christian Gil <cristhianalexisgil@gmail.com>
2019-11-13 15:22:45 -0500

    Rotator

--- MyLeasing.Prism/MyLeasing.Prism/App.xaml.cs
+++ MyLeasing.Prism/MyLeasing.Prism/App.xaml.cs
@@ -22,6 +22,7 @@ namespace MyLeasing.Prism
 
         protected override async void OnInitialized()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTcwMTAzQDMxMzcyZTMzMmUzMFlJYWlPWUF4UlVyNlRGYWxuN2c3dTlhUHMxaU1GMDZsc1VYaUo5OE9DUlU9");
             InitializeComponent();
 
             await NavigationService.NavigateAsync("NavigationPage/LoginPage");
--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyPageViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using MyLeasing.Common.Models;
 using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace MyLeasing.Prism.ViewModels
 {
     public class PropertyPageViewModel : ViewModelBase
     {
         private PropertyResponse _property;
+        private ObservableCollection<RotatorModel> _imageCollection;
 
         public PropertyPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = "Property";
+        }
+
+        public ObservableCollection<RotatorModel> ImageCollection
+        {
+            get => _imageCollection;
+            set => SetProperty(ref _imageCollection, value);
         }
 
         public PropertyResponse Property 
@@ -26,7 +36,18 @@ namespace MyLeasing.Prism.ViewModels
             {
                 Property = parameters.GetValue<PropertyResponse>("property");
                 Title = $"Property: {Property.Neighborhood}";
+                LoadImage();
             }
+        }
+        private void LoadImage()
+        {
+            var list = new List<RotatorModel>();
+            foreach (var propertyImage in Property.PropertyImages)
+            {
+                list.Add(new RotatorModel { Image = propertyImage.ImageUrl });
+            }
+
+            ImageCollection = new ObservableCollection<RotatorModel>(list);
         }
     }
 }
